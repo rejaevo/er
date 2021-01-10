3c3198ac5616e07f4044fb5179dd24211f670b83
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 01:06:21 +0300

    Updates

--- precache-manifest.d1e24c7d5bce32b8385779a35a1648e4.js
+++ precache-manifest.d1e24c7d5bce32b8385779a35a1648e4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d341c260b7d2594353b8241aaf638cc",
+    "revision": "209245b8e489585468ba3d6de2a61c95",
     "url": "./index.html"
   },
   {
-    "revision": "5bdbe48349588f832bb4",
+    "revision": "4cf61143f72d21926ec9",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "63fb921f1eb45cadf70a",
+    "revision": "5676e80b8c469b04ec51",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "5bdbe48349588f832bb4",
-    "url": "./static/js/2.d699bbc9.chunk.js"
+    "revision": "4cf61143f72d21926ec9",
+    "url": "./static/js/2.02b776d4.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.d699bbc9.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.02b776d4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "63fb921f1eb45cadf70a",
-    "url": "./static/js/main.0c521ee1.chunk.js"
+    "revision": "5676e80b8c469b04ec51",
+    "url": "./static/js/main.98e4b334.chunk.js"
   },
   {
     "revision": "fee87ee8dc7dcfa79dd1",
